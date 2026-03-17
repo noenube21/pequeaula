e2e285f34063c6525e3584a36655f2ba08f46c8e
noenube21 <noeminoee22@gmail.com>
2026-03-17 13:28:34 +0100

    Update juegos-master.js

--- js/juegos-master.js
+++ js/juegos-master.js
@@ -4,69 +4,33 @@ import {
 } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 import { comprobarRecompensas } from "./recompensas.js";
 
-// ================== GUARDAR PROGRESO ==================
-async function guardarProgreso(asignatura, aciertos, errores) {
-    const u = auth.currentUser;
-    if (!u) return;
-
-    const ref = doc(db, "usuarios", u.uid);
-    const snap = await getDoc(ref);
-    let d = snap.exists() ? snap.data() : {};
-
-    if (!d.progreso) d.progreso = {};
-    if (!d.progreso[asignatura]) {
-        d.progreso[asignatura] = { puntos: 0, completado: false };
-    }
-
-    await setDoc(ref, {
-        partidas: increment(1),
-        aciertos: increment(aciertos),
-        errores: increment(errores),
-        progreso: {
-            ...d.progreso,
-            [asignatura]: {
-                puntos: (d.progreso[asignatura].puntos || 0) + aciertos,
-                completado: aciertos > errores
-            }
-        }
-    }, { merge: true });
-
-    const tot = (d.aciertos || 0) + aciertos;
-    await comprobarRecompensas(tot);
-
-    const puntosTotales = ((d.aciertos || 0) + aciertos) - ((d.errores || 0) + errores);
-    const nivel = Math.max(1, Math.floor(puntosTotales / 10));
-    await updateDoc(ref, { nivel });
-}
-
 // ================== JUEGOS ==================
 const Juegos = {
+
     ingles3: {
         memory: [
-            ["dog","/assets/img/dog.png"],
-            ["house","/assets/img/house.png"],
-            ["apple","/assets/img/apple.png"]
+            ["dog","../assets/img/dog.png"],
+            ["house","../assets/img/house.png"],
+            ["apple","../assets/img/apple.png"]
         ]
     },
 
     ciencias3: {
         memory: [
-            ["sol","/assets/img/sol.png"],
-            ["luna","/assets/img/luna.png"],
-            ["mar","/assets/img/mar.png"]
+            ["sol","../assets/img/sol.png"],
+            ["luna","../assets/img/luna.png"],
+            ["mar","../assets/img/mar.png"]
         ]
     }
 };
 
 // ================== VARIABLES ==================
 let juegoActual = null;
-let asignaturaActual = null;
 let memoryCartas = [];
 let memorySeleccion = [];
 
 // ================== INICIAR ==================
 export function iniciarJuego(key) {
-    asignaturaActual = key;
     juegoActual = Juegos[key];
 
     const contenedor = document.getElementById("pregunta");
